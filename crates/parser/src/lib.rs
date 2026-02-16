b5c074ce0e5e7dd3f49f7feb2056719721ceec8c
Alan Mitchell <mitchellalan1999@icloud.com>
2026-02-15 19:15:52 -0600

    update: moved StructSection enum to parser.rs

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -4,11 +4,3 @@ pub mod loader;
 
 pub use ast::*;
 pub use errors::HydraError as ParserError;
-
-#[derive(PartialEq, PartialOrd, Clone, Copy)]
-pub enum StructSection {
-    NONE = 0,
-    CONSTANTS = 1,
-    FIELDS = 2,
-    METHODS = 3,
-}
